44c3600cdc8be47b40c6b95e05fdbefac259f746
ykk <ykrial@163.com>
2026-05-19 20:28:16 +0800

    fix: fedoraproject.org 直连，避免 Metalink 走代理分配到境外镜像导致 SSL 重置

--- proxy-override.js
+++ proxy-override.js
@@ -410,6 +410,8 @@ function injectRules(config) {
   for (var d = 0; d < devDomains.length; d++) {
     R.push('DOMAIN-SUFFIX,' + devDomains[d] + ',节点选择')
   }
+  // Fedora 镜像直连（Metalink 根据来源 IP 自动分配国内镜像，走代理反而被分到境外）
+  R.push('DOMAIN-SUFFIX,fedoraproject.org,DIRECT')
   R.push('GEOSITE,category-dev,节点选择')
 
   // 苹果服务
